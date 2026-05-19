60c907f473b44082db2c8d9bdf4428e27fb7873f
RasoLdev <chiki.ee.cot@gmail.com>
2026-05-19 21:41:49 +0200

    sm

--- src/supabase.jsx
+++ src/supabase.jsx
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const SUPABASE_URL = "https://yxlslbzwszivdpqbxhyy.supabase.co/rest/v1/"
+const SUPABASE_URL = "https://yxlslbzwszivdpqbxhyy.supabase.co"
 const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inl4bHNsYnp3c3ppdmRwcWJ4aHl5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzkyMTA0NjIsImV4cCI6MjA5NDc4NjQ2Mn0.oJLc52GAyskoToAYEeUiYgF-oyISRLUr_n3_Obxraos"
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY)
